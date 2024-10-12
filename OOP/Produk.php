d939060162edefee15b24c56630416e9f5494708
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-12 07:24:04 +0700

    OOP - Property dan Method

--- OOP/Produk.php
+++ OOP/Produk.php
@@ -1,9 +1,39 @@
 <?php
 
-// Jualan Produk
-// Komik
-// Game
-class Produk {}
+class Produk
+{
+    public  $judul = "judul",
+        $penulis = "prnulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk1 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "wkwkwkwkwkwkwk";
+// var_dump($$produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
