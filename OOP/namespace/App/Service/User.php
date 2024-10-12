e8826f753552f919e6dec60701be0e2f4dd778b9
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-12 19:37:57 +0700

    OOP - Namespace

---
+++ OOP/namespace/App/Service/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class user
+{
+    public function __construct()
+    {
+        echo "ini adalah class " . __CLASS__;
+    }
+}
---
+++ OOP/namespace/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "shonen Jump", 30000, 100);
+// $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 25000, 50);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+
+
+// echo "<hr>";
+
+// new coba();
+
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
