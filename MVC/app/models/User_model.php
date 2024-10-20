78d0e95a36c5ccd026bab39aa002c4e846a3389b
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-20 17:43:48 +0700

    MVC - Model

---
+++ MVC/app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model
+{
+    private $nama = 'Rafly Alfarizi';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
--- MVC/app/views/home/index.php
+++ MVC/app/views/home/index.php
@@ -1,8 +1,8 @@
 <div class="container ">
     <div class="card mt-4">
         <div class="card-body">
-            <h5 class="card-title">Selamat datang di website saya</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h1 class="card-title">Selamat datang di website saya</h1>
+            <p class="card-text">Halo, nama saya <?= $data['nama']; ?></p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
