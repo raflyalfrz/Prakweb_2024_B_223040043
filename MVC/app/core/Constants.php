9d776a861323db07751437a8da232f90e2c0e506
Renaajl <muhammadraflyalfrz24@gmail.com>
2024-10-20 16:52:24 +0700

    MVC - Asset

---
+++ MVC/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/Prakweb_2024_B_223040043/MVC/public');
--- MVC/app/views/home/index.php
+++ MVC/app/views/home/index.php
@@ -1,1 +1,10 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container ">
+    <div class="card mt-4">
+        <div class="card-body">
+            <h5 class="card-title">Selamat datang di website saya</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+
+</div>
